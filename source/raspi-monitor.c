1cfc07d1bc8bfb7d44cc28644f61e91df13460a0
s3rgi0s <bertagnollimarco999@gmail.com>
2022-10-03 16:34:21 +0200

    created basic menu funciton

--- source/raspi-monitor.c
+++ source/raspi-monitor.c
@@ -1,21 +1,94 @@
 //  Created by Marco Bertagnolli on 03/10/22.
 
 #include "raspi-monitor.h"
-#include "stdio.h"
-#include "stdlib.h"
-
 
 char banner[] = "                       _                       _                     __   ______   \n                      (_)                     (_)_                  /  | / __   |  \n  ____ ____  ___ ____  _ ___ ____   ___  ____  _| |_  ___   ____   /_/ || | //| |  \n / ___) _  |/___)  _ \\| (___)    \\ / _ \\|  _ \\| |  _)/ _ \\ / ___)    | || |// | |  \n| |  ( ( | |___ | | | | |   | | | | |_| | | | | | |_| |_| | |        | ||  /__| |  \n|_|   \\_||_(___/| ||_/|_|   |_|_|_|\\___/|_| |_|_|\\___)___/|_|        |_(_)_____/   \n                |_|                                                                \n";
 
 
-int menu(){
+// FILE PARSER
+
+void openVpnFileParser(char completePath[]){
+    FILE *vpnLogFile
+    vpnLogFile = fopen(completePath, "r");
+    
+    // read info
+    
+    
+    fclose(vpnLogFile);
+
+    return;
+}
+
+
+// MENU FUNCTIONS
+
+void config(){
     system("clear");
     printf("%s", banner);
+    printf("\n\nconfig> ");
+    
+    int menuConfOption;
+    scanf("%d", &menuConfOption);
+    
+}
+
+void serverStatus(){
+    system("clear");
+    printf("%s", banner);
+    printf("\n\nserverStatus> ");
+    
+    int menuServSOption;
+    scanf("%d", &menuServSOption);
+    
+}
+
+void serverInfo(){
+    system("clear");
+    printf("%s", banner);
+    printf("\n\nserverInfo> ");
+    
+    int menuServIOption;
+    scanf("%d", &menuServIOption);
+    
+}
+
+void updateCheck(){
+    menu("Alredy updated!");
+}
+
+int menu(char message[]){
+    system("clear");
+    printf("%s", banner);
+    printf("\n1.Configuration      2.Server start/stop  \n3.Server info        4.Ceck update  \n\n");
+    printf("%s", message);
+    printf("\n> ");
+    
+    int menuOption;
+    scanf("%d", &menuOption);
+    
+    switch(menuOption){
+        case 1:
+            config();
+            break;
+        case 2:
+            serverStatus();
+            break;
+        case 3:
+            serverInfo();
+            break;
+        case 4:
+            updateCheck();
+            break;
+        case 0:
+            break;
+        default:
+            menu("Inexistring option");
+    }
 }
 
 void main(){
-    int exitCode = menu();
-    printf("\\nBye!  exit code: %d", exitCode);
+    int exitCode = menu("Welcome!");
+    printf("\nBye!  =>  exit code: %d\n", exitCode);
 }
 
 
--- source/raspi-monitor.h
+++ source/raspi-monitor.h
@@ -1,13 +1,9 @@
-//
-//  raspi-monitor.h
-//  raspi-monitor
-//
 //  Created by Marco Bertagnolli on 03/10/22.
-//
 
 #ifndef raspi_monitor_h
 #define raspi_monitor_h
 
-#include <stdio.h>
+#include "stdio.h"
+#include "stdlib.h"
 
 #endif /* raspi_monitor_h */
