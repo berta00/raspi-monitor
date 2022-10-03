64ac2d50d66cd9fa8ffcbbe65766f8cb2cae0771
s3rgi0s <bertagnollimarco999@gmail.com>
2022-10-03 14:58:07 +0200

    Initial Commit

---
+++ raspi-monitor.c
@@ -0,0 +1,8 @@
+//
+//  raspi-monitor.c
+//  raspi-monitor
+//
+//  Created by Marco Bertagnolli on 03/10/22.
+//
+
+#include "raspi-monitor.h"
---
+++ raspi-monitor.h
@@ -0,0 +1,13 @@
+//
+//  raspi-monitor.h
+//  raspi-monitor
+//
+//  Created by Marco Bertagnolli on 03/10/22.
+//
+
+#ifndef raspi_monitor_h
+#define raspi_monitor_h
+
+#include <stdio.h>
+
+#endif /* raspi_monitor_h */
