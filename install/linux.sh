ab928de6026232aba1968f9aff8cee73011a18c5
s3rgi0s <bertagnollimarco999@gmail.com>
2022-10-06 18:34:37 +0200

    final final linux installer and updater

--- install/linux.sh
+++ install/linux.sh
@@ -1,8 +1,17 @@
 # raspi-monitor auto installation script
 
 if ls /bin/raspi-monitor; then
-    echo "already installed"
+    git pull
+    # compile for linux
+    gcc ./../source/raspi-monitor.c -w -o ./../bin raspi-monitor
+    # move to /bin directory
+    mv ./../bin/raspi-monitor /bin/raspi-monitor
+    echo "already installed and updated"
+    
 else
+    # compile for linux
+    gcc ./../source/raspi-monitor.c -w -o ./../bin raspi-monitor
+    # move to /bin directory
     mv ./../bin/raspi-monitor /bin/raspi-monitor
     echo "installed"
 fi
